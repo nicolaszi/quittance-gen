df3ceabb29f7b27d0ee0599faee4f0648646b0b4
nicolas.zi <nicolas.zilbermann@gmail.com>
2021-04-12 17:14:04 +0200

    chore: add css and image for signature

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
+import calendar
+from datetime import datetime
 import json
 from flask import Flask, render_template
 from flask_script import Manager, Command
 from flask_weasyprint import HTML, render_pdf
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 app = Flask(__name__)
 # configure your app
@@ -14,9 +16,18 @@ def create_quittance(date):
     "create pdf quittance"
     with open('data.json', 'r') as f:
         json_dict = json.load(f)
-    html = render_template('quittance.html')
-    pdf = render_pdf(HTML(string=html))
-    HTML(string=html).write_pdf('quittance.pdf')
+    splitdate = date.split('/')
+    month_range=calendar.monthrange(int(splitdate[1]),int(splitdate[0]))
+    firstday, lastday = month_range
+    formatted_firstday = "01/" + date
+    formatted_lastday = str(lastday) + "/" + date
+    format = "%d/%m/%Y"
+    today=datetime.now()
+    today_str = today.strftime(format)
+    for data in json_dict:
+        html = render_template('quittance.html', data=data, firstday=formatted_firstday, lastday=formatted_lastday, today=today_str)
+        pdf = render_pdf(HTML(string=html))
+        HTML(string=html, base_url='.').write_pdf('quittance.pdf',  stylesheets=[CSS(filename='static/style.css')])
 
 
 if __name__ == "__main__":
